e8e130f501ea4212dd6b236946d5a001d8a04202
lights0123 <me@lights0123.com>
2015-10-25 09:31:24 -0400

    Add brackets

---
+++ content/data.php
@@ -0,0 +1,11 @@
+CSC Curling | View Data
+<div>
+	<script src="/js/jquery.bracket.min.js"></script>
+	<script src="/js/fabric.js"></script>
+	<script src="/js/svg.js"></script>
+	<link rel='stylesheet' type='text/css' href='/css/jquery.bracket.min.css' />
+<?php
+chdir(__DIR__);
+include("../scripts/parse.php");
+?>
+</div>
---
+++ data.php
@@ -0,0 +1,3 @@
+<?php
+require_once("scripts/template.php");
+createPage("content/data.php", "data");
